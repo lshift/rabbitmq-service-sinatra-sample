5b43c780c3deb2c3e95011a63be35e628cf60ee3
Frank Shearar <frank@lshift.net>
2013-04-25 15:08:52 +0100

    Separate the producer from the consumer.

--- rabbit.rb
+++ rabbit.rb
@@ -6,35 +6,49 @@ require 'bunny'
 
 enable :sessions
 
-# Opens a client connection to the RabbitMQ service, if one isn't
+# Opens a producer connection to the RabbitMQ service, if one isn't
 # already open.  This is a class method because a new instance of
 # the controller class will be created upon each request.  But AMQP
 # connections can be long-lived, so we would like to re-use the
 # connection across many requests.
-def client
-  unless $client
-    c = Bunny.new(ENV['RABBITMQ_BIGWIG_URL'])
+def producer
+  unless $producer
+    c = Bunny.new(ENV['RABBITMQ_BIGWIG_TX_URL'])
     c.start
-    $client = c
+    $producer = c
+  end
+  $producer
+end
+
+# Opens a consumer connection to the RabbitMQ service, if one isn't
+# already open.  This is a class method because a new instance of
+# the controller class will be created upon each request.  But AMQP
+# connections can be long-lived, so we would like to re-use the
+# connection across many requests.
+def consumer
+  unless $consumer
+    c = Bunny.new(ENV['RABBITMQ_BIGWIG_RX_URL'])
+    c.start
+    $consumer = c
 
     # We only want to accept one un-acked message
-    $client.qos :prefetch_count => 1
+    $consumer.qos :prefetch_count => 1
   end
-  $client
+  $consumer
 end
 
 # Return the "nameless exchange", pre-defined by AMQP as a means to
 # send messages to specific queues.  Again, we use a class method to
 # share this across requests.
 def nameless_exchange
-  $nameless_exchange ||= client.exchange('')
+  $nameless_exchange ||= producer.exchange('')
 end
 
 # Return a queue named "messages".  This will create the queue on
 # the server, if it did not already exist.  Again, we use a class
 # method to share this across requests.
 def messages_queue
-  $messages_queue ||= client.queue("messages")
+  $messages_queue ||= consumer.queue("messages")
 end
 
 def take_session key
